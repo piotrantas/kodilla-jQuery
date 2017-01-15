4ca4be306404a60010fac99195f08b0011040d8d
piotrantas <antas.piotr@gmail.com>
2017-01-15 20:37:54 +0100

    add carousel 2/3

--- scripts.js
+++ scripts.js
@@ -1,3 +1,30 @@
 $(function(){
+	var carouselList = $("#carousel .image");
+	var interval = setInterval(changeSlide, 3000);
+
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-400}, 200, moveFirstSlide);
+	}
+	function moveFirstSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.after(firstItem);
+		carouselList.css({marginLeft:0});
+	}
 	
+	$('#right-arrow').click(changeSlide);
+	
+
+	$('#left-arrow').click(function(){
+		carouselList.animate({'marginLeft': 400}, 500, moveLastSlide);
+	})
+
+	function moveLastSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		firstItem.before(lastItem);
+		carouselList.css({marginLeft:0});
+	}
+
+
 });
