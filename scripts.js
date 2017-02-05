747727aff644baf8c6362e558654268679831687
piotrantas <antas.piotr@gmail.com>
2017-02-05 14:58:15 +0100

    improved left click

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 $(function(){
 	var carouselList = $("#carousel .image");
-	var interval = setInterval(changeSlide, 3000);
+	var interval = setInterval(changeSlide, 5000);
 
 	function changeSlide() {
 		carouselList.animate({'marginLeft':-400}, 200, moveFirstSlide);
@@ -16,14 +16,15 @@ $(function(){
 	
 
 	$('#left-arrow').click(function(){
-		carouselList.animate({'marginLeft': 400}, 500, moveLastSlide);
+		moveLastSlide();
+		carouselList.animate({'marginLeft': 0}, 200);
 	})
 
 	function moveLastSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		firstItem.before(lastItem);
-		carouselList.css({marginLeft:0});
+		carouselList.css({marginLeft:400});
 	}
 
 
